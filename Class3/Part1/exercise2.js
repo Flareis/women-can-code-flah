2373ce507b55f7e516588f11cf50d588f0efe8e2
FlaviaDosReis <reis.dos.flavia@gmail.com>
2021-11-10 23:31:56 -0300

    Update folder name

---
+++ Class3/Part1/exercise2.js
@@ -0,0 +1,11 @@
+/*2. Vamos explorar a criação de variáveis sem inicializa-las com um valor! 
+Coloque como comentário no código o que acontece quando você executa os seguintes trechos de código:*/
+
+var meuNome
+console.log(meuNome)
+/*Quando imprimo aparece undefined, pois não há variavél definida, 
+ou seja, sem valor dentro da variável.*/
+
+meuNome = "Flavia"
+console.log(meuNome) 
+/*Quando imprimo aparece o conteúdo da variável, pois está definida.*/
---
+++ Class3/Part1/exercise4.js
@@ -0,0 +1,10 @@
+/* 4.Depois que um valor é atribuído a uma variável usando o operador de atribuição , você pode
+ atribuir o valor dessa variável a outra variável usando o operador de atribuição, como no exemplo a seguir:*/
+
+var salarioBase; 
+salarioBase = 2000;
+var salarioFuncionarioNovo;
+salarioFuncionarioNovo = salarioBase
+
+console.log(salarioBase)
+console.log(salarioFuncionarioNovo)
