09abf728428c66722c84284fd216004b507cbc1a
FlaviaDosReis <reis.dos.flavia@gmail.com>
2021-11-13 17:29:06 -0300

    Update Class6

---
+++ Class6/explanations13.11.js
@@ -0,0 +1,4 @@
+/* Na aula 6 foi criado novo repositório para criação do projeto: Servidor.
+Usamos HTTP, JSON, API REST e NPM.
+
+Acessar o repositório no link: https://github.com/Flareis/WCC_6_node_api_Flareis.git*/
